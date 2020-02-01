a112e3ae00af55ebf29df036ee9bdb83f4f353a4
jyeh20 <jmyeh51@gmail.com>
2020-02-01 11:44:58 -0800

    -initializing js files

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import firebase from './lib/firebase';
+import {useEffect, useState} from 'react';
+import Person from './Person';
+import Register from './Register';
+import './App.css';
+import Home from './Home';
+import Users from  './Users';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function Function() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
---
+++ src/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Person.css';
+import {useEffect, useState} from 'react';
+
+
+// props = {item, blah}
+function Person(props) {
+  return (
+    <div className="Person">
+      <h1>{props.item.name}</h1>
+      <h2>{props.item.age}</h2>
+      <h2>{props.item.school}</h2>
+      <h2>{props.item.year}</h2>
+      <h2>{props.item.pronouns}</h2>
+      <h2>{props.item.interests}</h2>
+    </div>
+  );
+}
+
+export default Person;
